c2d67890675bbcc30970d389a3e356c7c597b4a1
Dana Marin <d_maringarcia@fanshaweonline.ca>
2020-04-05 17:53:21 -0400

    adding styling + success and error pages to the contact form

--- mail.php
+++ mail.php
@@ -1,6 +1,5 @@
 <?php
 if(empty($_POST)){ 
-    echo 'No...';
     exit;
 }
     $name = '';
@@ -23,39 +22,14 @@ if(empty($_POST)){
         $message = $_POST['message'];
     }
 
-    // $headers = [
-    //     'From' => 'noreply@test.ca',
-    //     'Reply-To' =>$name.'<'.$email.'>'
-    // ];
+    $headers = [
+        'From' => 'danamarin.com',
+        'Reply-To' =>$name.'<'.$email.'>'
+    ];
 
-    if(mail($recipient, $message, $headers)){
-        echo '<p>Thank you for contacting me, '.$name.'. You will get a reply within 24 hours</p>';
-    }else{
-        echo '<p>We are sorry but the contact form is temporarily disabled. Please send a direct email to dana.macarg@gmail.com or try again later. Thanks!</p>';
-    }
+    if(mail($recipient, 'New message!', $message, $headers)){
+            header("Location: http://danamarin.com/message_sent.html");
+        }else {
+            header("Location: http://danamarin.com/error.html");
+        }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-
-    <!-- CSS links -->
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/main.css">
-
-    <!-- Fonts links -->
-    <link rel="stylesheet" href="https://use.typekit.net/fdq3zps.css">
-    <title>Dana Marin Portfolio</title>
-</head>
-<body>
-    <button id="form-error">
-        <a href="index.html">
-            back to home
-        </a>
-    </button>
-</body>
-</html>
